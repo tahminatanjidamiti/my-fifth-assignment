1a6896a3b8af5ecc330232de2dad5d8ffdc8c813
tahminatanjidamiti <mity2027@gmail.com>
2024-09-27 02:29:42 +0600

    modifed and added new section

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 function goBlog(){
     window.location.href ='blog.html'
 }
+function goIndex(){
+    window.location.href ='index.html'
+}
 
-// const historyTab = document.getElementById('show-active-history');
-// const donationTab = document.getElementById('show-active-donation');
-//  historyTab.addEventListener('click', function(){
-//     historyTab.classList.add('text-black', 'bg-[#B4F461]', 'font-semibold', 'text-xl');
-//     history.
-
-//  });
 
 let activeButton = document.getElementById('show-active-donation');
 
